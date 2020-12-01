12ae888163729416f094e9933e5beeedeb556ae7
Erich Dalton <erich.dalton@ozmo.com>
2020-12-01 11:38:51 -0500

    improvement: refactor code to as close to bare essentials as known.
    chore: update readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # advent-of-code-2020
 Time to learn Elixir until I get bored or overwhelmed!
+
+## Description
+This repository houses my [Advent of Code 2020](https://adventofcode.com/2020) attempts.
+Each day a challenge is posted to solve in a language of your choosing, as a sort of
+super-kata thing to warm up for the day. They...frequently get out of hand quickly.
+
+## Prerequisites
+You must [install Elixir](https://elixir-lang.org/install.html) to your machine. 
+At time of writing, I am using Elixir 1.10.4 on Erlang/OTP 23.
+
+## Running a day's code.
+Currently, each day is written as a simple `.exs` script file. This means, to run the code,
+all one needs to do is run `elixir [filename]` and get the output to their terminal. Each day
+will have any needed input alongside it.
+
+IE: 
+```bash
+➜  day-1 git:(trunk) ✗ elixir day1.exs
+Passcode is 259716.
+Second passcode is 120637440.
+```
+
+Later days may very well begin to use `mix` dependencies, and the README will be updated
+accordingly if that occurs.
--- day-1/day1.exs
+++ day-1/day1.exs
@@ -2,33 +2,21 @@ defmodule Advent2020.Day1 do
   @sum_target 2020
 
   def main do
-    {:ok, file} = File.read("input.txt")
-    numbers = to_numbers(String.split(file, "\n"))
-
-    passcode = Enum.reduce(sum_set(numbers), &*/2)
-    passcode2 = Enum.reduce(sum_set(numbers, 3), &*/2)
-
-    IO.puts("Passcode is #{passcode}.")
-    IO.puts("Second passcode is #{passcode2}.")
+    nums = Enum.map(String.split(File.read!("input.txt"), "\n"), &String.to_integer/1)
+    IO.puts("Passcode is #{nums |> sum_set |> product}.")
+    IO.puts("Second passcode is #{nums |> sum_set(3) |> product}.")
   end
 
-  defp to_numbers(numbers) do
-    numbers
-      |> Enum.map(&Integer.parse/1)
-      |> Enum.map(fn {num, _rem} -> num end)
-  end
-
-  defp sum_set(numbers, num_matches \\ 2) do
-    Enum.filter(numbers, fn n -> sums_to?(n, numbers, num_matches) end)
-  end
-
-  def sums_to?(n, list, num_matches) do
+  defp sums_to?(n, list, num_matches) do
     case num_matches do
       0 -> false
       1 -> n === @sum_target
-      _ -> Enum.any?(list, fn num -> sums_to?(num + n, list, num_matches - 1) end)
+      _ -> Enum.any?(list, &(sums_to?(&1 + n, list, num_matches - 1)))
     end
   end
+
+  defp sum_set(numbers, num_matches \\ 2), do: Enum.filter(numbers, &(sums_to?(&1, numbers, num_matches)))
+  defp product(ns), do: Enum.reduce(ns, &*/2)
 end
 
 Advent2020.Day1.main()
