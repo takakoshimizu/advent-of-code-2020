29c20465e267f873e97405290719333aa905769c
Erich Dalton <erich.dalton@ozmo.com>
2020-12-01 15:00:00 -0500

    boy howdy, guards AND matched function defs

--- day-1/day1.exs
+++ day-1/day1.exs
@@ -7,14 +7,10 @@ defmodule Advent2020.Day1 do
     IO.puts("Second passcode is #{nums |> sum_set(3) |> product}.")
   end
 
-  defp sums_to?(n, list, num_matches) when num_matches > 0 do
-    case num_matches do
-      1 -> n === @sum_target
-      _ -> Enum.any?(list, &(sums_to?(&1 + n, list, num_matches - 1)))
-    end
-  end
-
   defp sum_set(numbers, num_matches \\ 2), do: Enum.filter(numbers, &(sums_to?(&1, numbers, num_matches)))
+
+  defp sums_to?(n, _list, 1), do: n === @sum_target
+  defp sums_to?(n, list, num_matches) when num_matches > 1, do: Enum.any?(list, &(sums_to?(&1 + n, list, num_matches - 1)))
   defp product(ns), do: Enum.reduce(ns, &*/2)
 end
 
